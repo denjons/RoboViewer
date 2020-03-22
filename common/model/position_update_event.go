3fc0fcdb8ee69a0c4ae07e1f04e2bc0e601ad206
Dennis Jönsson <dennis.j@live.se>
2020-03-22 16:28:41 +0100

    updated robot and sessionId to string

--- common/model/position_update_event.go
+++ common/model/position_update_event.go
@@ -3,8 +3,8 @@ package model
 // PositionUpdateEvent is an internal represeantion of an position update from a robot
 type PositionUpdateEvent struct {
 	Sequence  int64
-	RobotID   int32
-	SessionID int32
+	RobotID   string
+	SessionID string
 	Position  *Position
 }
 
