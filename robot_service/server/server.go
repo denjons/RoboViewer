a0205c59c5801cfdff43ca3498f350faf3e20d92
Dennis Jönsson <dennis.j@live.se>
2020-04-02 18:47:49 +0200

    update robot service server with kafka listener

--- robot_service/server/server.go
+++ robot_service/server/server.go
@@ -7,19 +7,28 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"strings"
 
+	c "github.com/denjons/RoboViewer/common/kafka/consumer"
+	common "github.com/denjons/RoboViewer/common/model"
 	"github.com/denjons/RoboViewer/robot_service/client/model"
 	"github.com/denjons/RoboViewer/robot_service/database"
+	kafka "github.com/denjons/RoboViewer/robot_service/kafka"
 )
 
 var (
-	serverPort  = flag.Int("server.port", 8080, "Server bind port.")
-	serverHost  = flag.String("server.host", "localhost", "server bind host.")
-	dbURL       = flag.String("db.url", "postgres://robot_service:robot_service@localhost:5432/robot_service?sslmode=disable", "database url")
-	dbMigration = flag.String("db.migration", "file://robot_service/database/migrations", "db migration file location")
+	serverPort   = flag.Int("server.port", 8080, "Server bind port.")
+	serverHost   = flag.String("server.host", "localhost", "server bind host.")
+	dbURL        = flag.String("db.url", "postgres://robot_service:robot_service@localhost:5432/robot_service?sslmode=disable", "database url")
+	dbMigration  = flag.String("db.migration", "file://robot_service/database/migrations", "db migration file location")
+	kafkaHost    = flag.String("kafka.host", "localhost", "broker host")
+	kafkaTopic   = flag.String("kafka.topic", "position-events", "Topic for postion events")
+	kafkaGroupID = flag.String("kafka.groupid", "default", "Kafka group id")
 )
 
 func main() {
+
+	startKafkaListener()
 
 	log.Printf("Running database migrations")
 	if err := database.MigrateDatabase(*dbMigration, *dbURL); err != nil {
@@ -63,6 +72,21 @@ func main() {
 
 	log.Printf("Serving on %v:%v", *serverHost, *serverPort)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v:%v", *serverHost, *serverPort), nil))
+}
+
+func startKafkaListener() {
+	topics := strings.Split(*kafkaTopic, ",")
+	kafkaConsumer, err := c.NewKafkaConsumer(*kafkaHost, *kafkaGroupID, &topics)
+	if err != nil {
+		log.Fatalf("Error creating KafkaConsumer: %v", err)
+	}
+	listener, err := kafka.NewListener(kafkaConsumer)
+	if err != nil {
+		log.Fatalf("Error creating Listener: %v", err)
+	}
+	listener.ListenForPositionUpdateEvents(func(p *common.PositionUpdateEvent) {
+		log.Printf("Received message: %v", p)
+	})
 }
 
 func getBytes(r *http.Request) (*[]byte, error) {
